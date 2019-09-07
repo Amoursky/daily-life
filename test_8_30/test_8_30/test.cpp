c9b81526da07d2f425b92304acdbc8621a498e95
Amoursky <1161079681@qq.com>
2019-09-07 21:42:46 +0800

    提交

---
+++ test_8_30/test_8_30/test.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a = 0;
+	cout << "Please input the number : " ;
+	cin >> a;
+	cout << "The number is " << a ;
+	cout << "." << endl;
+	system("pause");
+	return 0;
+}       

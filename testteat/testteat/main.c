16caa16a837d54680b582f5507d2d8b3d7373bcf
赵凡 <zhaofan@zhaofandeMacBook-Pro.local>
2019-10-19 13:09:51 +0800

    test

---
+++ testteat/testteat/main.c
@@ -0,0 +1,22 @@
+//
+//  main.c
+//  testteat
+//
+//  Created by 赵凡 on 2019/10/19.
+//  Copyright © 2019 赵凡. All rights reserved.
+//
+
+#include <stdio.h>
+
+//int main(int argc, const char * argv[]) {
+//    // insert code here...
+//    printf("Hello, World!\n");
+//    return 0;
+//}
+
+
+int main()
+{
+    printf("This is a test!\n");
+    return 0;
+}

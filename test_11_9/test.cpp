7c53420c0836c7c200c2f2718350828eaaa0a11d
Amoursky <1161079681@qq.com>
2019-11-09 10:44:03 +0800

    提交

--- test_11_9/test.cpp
+++ test_11_9/test.cpp
@@ -5,8 +5,13 @@ using namespace std;
 void testString()
 {
     string s;
-    s.push_back('h');
-    s.append("hello");
+    string s2("keda");
+    //s.push_back('h');
+    s += 'e';
+    //s.append("hello");
+    s += "hello";
+
+    s += s2;
     cout << s << endl;
 }
 

725a68b123e6c7eba7807adc87d5cb5e9024c2a2
Amoursky <1161079681@qq.com>
2019-11-09 18:00:43 +0800

    提交

--- test_11_9/test.cpp
+++ test_11_9/test.cpp
@@ -1,22 +1,66 @@
 #include<iostream>
 #include<string>
+#include<vector>
 using namespace std;
 
-void testString()
-{
-    string s;
-    string s2("keda");
-    //s.push_back('h');
-    s += 'e';
-    //s.append("hello");
-    s += "hello";
+// void testString()
+// {
+//     string s;
+//     string s2("keda");
+//     //s.push_back('h');
+//     s += 'e';
+//     //s.append("hello");
+//     s += "hello";
 
-    s += s2;
-    cout << s << endl;
-}
+//     //s += s2;
+//     s2.assign(s);
+//     cout << s << endl;
+//     cout << s2 << endl;
+// }
 
-int main()
-{
-    testString();
-    return 0;
-}
+// // int main()
+// // {
+// //     //testString();
+// //     //string file("test.cpp.txt.zip");
+// //     //string s = file.substr(4,4);
+// //     // size_t pos = file.rfind('.');
+// //     // string s = file.substr(pos,file.size() - pos);
+// //     // cout << s << endl;
+// //     string addr("https://www.nowcoder.com/profile/677393000/tests");
+// //     size_t pos = addr.find("://");
+// //     string protocal = addr.substr(0,pos);
+// //     cout << protocal <<endl;
+// //     size_t end = addr.find('/',pos + 3);
+// //     string domain = addr.substr(pos + 3,end - pos - 3);
+// //     cout << domain << endl;
+
+// //     return 0;
+// // }
+
+// // class Solution {
+// // public:
+// //     void reverseString(string& s) 
+// //     {
+// //         if(s.size())
+// //         {
+// //         string::iterator p = s.begin();
+// //         string::iterator q = s.end() - 1;
+// //         while(p<q)
+// //         {
+// //             swap(*p,*q);
+// //             p++;
+// //             q--;
+// //         }
+// //         cout << s << endl;
+// //         }
+// //     }
+// // };
+
+// // int main()
+// // {
+// //     Solution a;
+// //     string s("hello world!");
+// //     //string s;
+// //     a.reverseString(s);
+// //     return 0;
+// // }
